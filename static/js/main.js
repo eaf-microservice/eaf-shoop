0791dd20bcaa28056ff6b60d8bf2a38da7001c30
eaf-microservice <EAF.microservice@gmail.com>
2026-01-20 00:05:46 +0100

    Add search and error pages

--- static/js/main.js
+++ static/js/main.js
@@ -1,5 +1,4 @@
 document.addEventListener('DOMContentLoaded', () => {
-
     const newProductsContainer = document.querySelector('.new-product-grid');
     const promotionsContainer = document.querySelector('.promotions-products-sidebar');
     const allProductsContainer = document.querySelector('.all-product-grid');
@@ -8,6 +7,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const createProductCard = (product) => {
         return `
             <div class="product-card" tabindex="0">
+                <div class="product-type-badge">${product.type}</div>
                 <div class="product-image">
                     <img src="${product.image}" alt="${product.name}">
                     <div class="product-overlay">
@@ -16,7 +16,7 @@ document.addEventListener('DOMContentLoaded', () => {
                 </div>
                 <h3>${product.name}</h3>
                 <p class="price">${product.price} MAD</p>
-                <button class="add-to-cart">Add to Cart</button>
+                <button class="add-to-cart">Ajouter au panier</button>
             </div>
         `;
     };
@@ -36,7 +36,7 @@ document.addEventListener('DOMContentLoaded', () => {
                     <span class="original-price">${product.original_price} MAD</span>
                     ${product.price} MAD
                 </p>
-                <button class="add-to-cart">Add to Cart</button>
+                <button class="add-to-cart">Ajouter au panier</button>
             </div>
         `;
     };
@@ -66,8 +66,12 @@ document.addEventListener('DOMContentLoaded', () => {
     const searchButton = document.getElementById('search-button');
 
     searchButton.addEventListener('click', () => {
-        const query = searchInput.value.toLowerCase();
-        sessionStorage.setItem('searchQuery', query);
-        window.location.href = 'search.html';
+        const query = searchInput.value.trim(); // Use trim to handle whitespace only input
+        if (query === '') {
+            alert('Aucun terme de recherche fourni.'); // Simple alert for error message
+            return; // Do nothing further
+        }
+        sessionStorage.setItem('searchQuery', query.toLowerCase());
+        window.location.href = '../pages/search.html';
     });
 });
--- static/js/search.js
+++ static/js/search.js
@@ -1,23 +1,27 @@
+const createProductCard = (product) => {
+    return `
+        <div class="product-card" tabindex="0">
+            <div class="product-type-badge">${product.type}</div>
+            <div class="product-image">
+                <img src="../${product.image}" alt="${product.name}">
+                <div class="product-overlay">
+                    <p class="product-description">${product.description || ''}</p>
+                </div>
+            </div>
+            <h3>${product.name}</h3>
+            <p class="price">${product.price} MAD</p>
+            <button class="add-to-cart">Ajouter au panier</button>
+        </div>
+    `;
+};
+
 document.addEventListener('DOMContentLoaded', () => {
     const searchResultsContainer = document.querySelector('.search-results-grid');
-    const createProductCard = (product) => {
-        return `
-            <div class="product-card" tabindex="0">
-                <div class="product-image">
-                    <img src="${product.image}" alt="${product.name}">
-                    <div class="product-overlay">
-                        <p class="product-description">${product.description || ''}</p>
-                    </div>
-                </div>
-                <h3>${product.name}</h3>
-                <p class="price">${product.price} MAD</p>
-                <button class="add-to-cart">Add to Cart</button>
-            </div>
-        `;
-    };
 
     const searchQuery = sessionStorage.getItem('searchQuery');
     let filteredProducts = [];
+
+    
 
     if (allProductsDB && allProductsDB.allProducts && searchQuery) {
         filteredProducts = allProductsDB.allProducts.filter(product => {
@@ -32,7 +36,9 @@ document.addEventListener('DOMContentLoaded', () => {
         filteredProducts.forEach(product => {
             searchResultsContainer.innerHTML += createProductCard(product);
         });
+        document.getElementById('search-query').textContent = searchQuery;
     } else {
-        searchResultsContainer.innerHTML = '<p>No products found for your search.</p>';
+        document.getElementById('search-query').textContent = searchQuery;
+        searchResultsContainer.innerHTML = '<p class="result-text">Aucun produit trouvé pour votre recherche.</p>';
     }
 });
